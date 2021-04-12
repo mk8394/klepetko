d816208326ab9664397e0b4db0cb52fb676625de
Tomaž Sagaj <ts8043@student.uni-lj.si>
2021-04-12 23:21:48 +0200

    Spawning.

--- client/networking.js
+++ client/networking.js
@@ -1,7 +1,7 @@
 let ctx = document.getElementById('ctx').getContext('2d');
 let username = document.getElementById('username');
 let connectBtn = document.getElementById('connectBtn');
-
+let actualCtx = document.getElementById('ctx');
 
 const registerEL = () => {
     if (!username.value) {
@@ -15,6 +15,10 @@ username.addEventListener('input', registerEL);
 const connect = () => {
     let socket = io();
 
+    document.getElementById('ctx').style.display = 'inline';
+    document.getElementById('connectBtn').style.display = 'none';
+    document.getElementById('username').style.display = 'none';
+
     if (username.value) {
         connectBtn.disabled = true;
         connectBtn.innerHTML = "Connected";
@@ -23,8 +27,15 @@ const connect = () => {
         socket.emit('register', username.value);
     }
 
+    // size of canvas object
+    var style = window.getComputedStyle(actualCtx);
+    var width = style.getPropertyValue('width');
+    var height = style.getPropertyValue('height');
+    var actualWidth = width.substring(0, width.length - 2);
+    var actualHeight = height.substring(0, height.length - 2);
+
     socket.on('spawn', (player) => {
-        ctx.fillText(player.username, Math.random() * 100, Math.random() * 100);
+        ctx.fillText(player.username, actualWidth/2, actualHeight/2);
         console.log(player);
     });
 }
